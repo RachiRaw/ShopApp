ef58e76b176fe7fd08bcfb6598c7bd7bbd478052
RachiRaw <131473716+RachiRaw@users.noreply.github.com>
2023-11-23 22:02:26 +0530

    Added env file and connected client to proxy for server

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,22 @@ import express from "express";
 import products from "./data/products.js";
 
 const app = express();
+import dotenv from "dotenv";
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Api is running");
 });
 
-const PORT = 5000;
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((product) => product._id === req.params.id);
+  res.json(product);
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is up at port ${PORT}`));
