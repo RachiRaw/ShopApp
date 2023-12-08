fe3a0c2999cfe052f983af7c39037e4b4dce80bc
RachiRaw <131473716+RachiRaw@users.noreply.github.com>
2023-12-08 21:41:51 +0530

    Added new product and product edit functionality from admin

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,15 @@ const router = express.Router();
 import {
   getProducts,
   getProductById,
+  createProduct,
+  updateProduct,
 } from "../controllers/productController.js";
+import { protectRoutes, admin } from "../middleware/authmiddleware.js";
 
-router.route("/").get(getProducts);
-router.route("/:id").get(getProductById);
+router.route("/").get(getProducts).post(protectRoutes, admin, createProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .put(protectRoutes, admin, updateProduct);
 
 export default router;
